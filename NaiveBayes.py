89c1bd47a4c9bf32a0dc72323fbaa824f15ec774
Simon Roth <simonroth909@gmail.com>
2022-12-08 17:27:29 +0100

    redid naive bayes classifier

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -2,47 +2,40 @@ import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
 
 class NaiveBayes:
-    probabilities = {}
+    likelihoods = {}
     fitted = False
-    target_vals = []
+    classes = []
     length = 0
+    priors = {}
 
-    def fit(self, dataset, target):
-        outcomes = []
-        self.length = len(dataset[target])
-        occurrences = dataset[target].value_counts()
-        for val in dataset[target].unique():
-            self.target_vals.append((val,
-                                     occurrences.val))  # should store for each target how often it occurs in format (value, occurrences)
-            outcomes.append(pd.DataFrame(dataset[dataset[target] == val]))
-        for outcome in outcomes:
-            curr_val = outcome[target].iat[0]
-            for col in outcome.columns:
-                length = len(outcome[col])
-                for val, cnt in outcome[col].value_counts().items():
-                    self.probabilities[(col, val, curr_val)] = (cnt + 1) / length  # TODO zero frequency problem
+    def fit(self, x, y):
+        self.classes = y
+        df = x.join(y)
+        self.length = len(y)
+        for c in self.classes.unique():
+            count = self.classes.value_counts()[c]
+            self.priors[c] = count / self.length  # get prior probability for each class and store in a dict
+            for col in x.columns:
+                for val, cnt in (df.loc[df[y.name] == c])[col].value_counts().items():
+                    self.likelihoods[(col, val, c)] = (cnt + 1) / count  # offset by 1 for zero frequency problem
         self.fitted = True
 
     # TODO probably lot of mistakes
-    def predict(self, input):
+    def predict(self, x):
         if not self.fitted:
-            print("Fit Classifier before predicting")
-            return
+            print("Fit before predicting")
+            return x
+        max_p = -1
+        current_guess = self.classes[0]
         res = []
-        normalization = 0
-        p = 0
-        for val, occ in self.target_vals:
-            for row in input.iterrows:  # iterrows very inefficient, optimise with different method
-                p = 0
-                for col in input.columns:
-                    if p == 0:
-                        p = self.probabilities[col, input[row][col], val]
-                    else:
-                        p *= self.probabilities[col, input[row][col], val]
-            res.append((val, p))
-            normalization += p
-        # normalization
-        normalized_res = {}
-        for x, y in res:
-            normalized_res[x] = y / normalization  # might be completely wrong
-        return normalized_res
+        for i, row in x.iterrows():
+            for c in self.classes.unique():
+                p = self.priors[c]
+                for col in x.columns:
+                    p *= self.likelihoods.get((col, row[col], c), 1 / self.length) #default occurence is 1
+                if p > max_p:
+                    max_p = p
+                    current_guess = c
+            res.append(current_guess)
+            max_p = -1
+        return res
